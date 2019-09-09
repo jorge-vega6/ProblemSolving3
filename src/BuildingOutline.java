c66d379f69e3cb03cb44b5a33ed08f2dc296802c
jorge-vega6 <jorge.vega6@upr.edu>
2019-09-09 12:01:46 -0400

    Initial commit

---
+++ src/BuildingOutline.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+
+class BuildingOutline {
+  public static void main(String[] args) {
+		int[][] grid = {{2,0,4},{3,8,5},{1,5,7}};
+		int[][] grid2 = {{3,4,5,7}, {9,27,36,1}, {1,4,5,6}, {3,50,1,3}};
+		System.out.println(maxOutline(grid));
+	}
+	
+	public static int maxOutline(int[][] grid) {
+		
+		int maxSum = 0;
+		
+		//rows: ArrayList to house the max values of the corresponding rows in the grid
+		//cols: ArrayList to house the max values of the corresponding rows in the grid
+		ArrayList<Integer> rows = new ArrayList<Integer>();
+		ArrayList<Integer> cols = new ArrayList<Integer>();
+		
+		for(int i = 0; i < grid.length; i++) {
+			rows.add(0);
+		}
+		
+		for(int i = 0; i < grid[0].length; i++) {
+			cols.add(0);
+		}		
+		
+		//filling each ArrayList with its respective max values 
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[i].length; j++) {
+				if (grid[i][j] > rows.get(i)) {
+					rows.set(i, grid[i][j]);
+				}
+				if (grid[i][j] > cols.get(j)) {
+					cols.set(j, grid[i][j]);
+				}
+			}
+		}
+		
+		//this gets the max value of the current element's column and row to then choose the lowest one and add to maxSum the amount the element needs to reach it
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[i].length; j++) {
+				int leastMax = rows.get(i);
+				if (cols.get(j) < leastMax) leastMax = cols.get(j);
+				if (grid[i][j] < leastMax) {
+					maxSum += leastMax - grid[i][j];
+				}
+				
+			}
+		}
+		
+		return maxSum;  
+	}
+}
---
+++ src/TransposeMatrix.java
@@ -0,0 +1,62 @@
+import java.util.Stack;
+
+public class TransposeMatrix {
+
+	public static void main(String[] args) {
+		//change to 'matrix' the name of the 2d array you want to rotate
+		int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+		int[][] matrix2 = {{5,1,9,11},{2,4,8,10},{13,3,6,7},{15,14,12,16}};
+		
+		//Printing the original matrix
+		System.out.println("Original: ");
+		for(int i = 0; i < matrix.length; i++) {
+			System.out.print("[");
+			for(int j = 0; j < matrix[0].length; j++) {
+				System.out.print(matrix[i][j]);
+				if(j != matrix[0].length - 1) {
+					System.out.print(", ");
+				}
+			}
+			System.out.print("]");
+		}
+		
+		rotate(matrix);
+		
+		//Printing the rotated matrix
+		System.out.println();
+		System.out.println("Rotated: ");
+		for(int i = 0; i < matrix.length; i++) {
+			System.out.print("[");
+			for(int j = 0; j < matrix[0].length; j++) {
+				System.out.print(matrix[i][j]);
+				if(j != matrix[0].length - 1) {
+					System.out.print(", ");
+				}
+			}
+			System.out.print("]");
+		}
+	}
+
+	//desired outcome for matrix1: [7,4,1],[8,5,2],[9,6,3]
+	//desired outcome for matrix2: [15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]
+	
+	static void rotate(int[][] matrix) {
+		
+		Stack<Integer> stack = new Stack<Integer>();
+		
+		//push all columns from each row into the stack
+		for(int i = 0; i < matrix.length; i++) {
+			for(int j = 0; j < matrix[0].length; j++) {
+				stack.push(matrix[i][j]);
+			}
+		}
+		
+		//set the values for each row in the same column to what comes out of the stack
+		for(int i = 0; i < matrix[0].length; i++) {
+			for(int j = matrix.length - 1; j >= 0; j--) {
+				matrix[j][i] = stack.pop();
+			}
+		}
+	}
+
+}
